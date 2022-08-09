d3d132cbbe8603a6419759fc8247d32053096782
shegezzy <39660743+shegezzy@users.noreply.github.com>
2022-08-09 11:19:26 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿namespace jaggedarray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[,,] firstArray = new int[4, 3, 2]
+            {
+
+                { { 4, 5 }, { 8, 6 }, { 8, 7 } },
+                { { 4, 5 }, { 8, 6 }, { 8, 7 } },
+                { { 4, 5 }, { 8, 6 }, { 8, 7 } },
+                { { 4, 5 }, { 8, 6 }, { 8, 7 } },
+            };
+
+            int[,,] secondArray = new int[4, 3, 2]
+           {
+
+                { { 7, 8 }, { 6, 5 }, { 3, 5 } },
+                { { 7, 8 }, { 6, 5 }, { 3, 5 } },
+                { { 7, 8 }, { 6, 5 }, { 3, 5 } },
+                { { 7, 8 }, { 6, 5 }, { 3, 5 } },
+           };
+            int[,,] answer = new int[4, 3, 2];
+            for (int x = 0; x < firstArray.GetLength(0); x++)
+            {
+                for (int y = 0; y < firstArray.GetLength(1); y++)
+                {
+                    for (int z = 0; z < firstArray.GetLength(2); z++)
+                    {
+                        answer[x, y, z] += secondArray[x, y, z] * firstArray[x, y, z];
+                        Console.WriteLine(answer[x, y, z]);
+                    }
+
+                }
+
+
+            }
+
+
+
+
+        }
+    }
+}
